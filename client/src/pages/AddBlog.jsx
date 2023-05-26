b7e37010d9a4b4798fe17d5b248f07e942f5df02
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-26 16:17:16 -0400

    setup a basic form for posting a blog.

--- client/src/pages/AddBlog.jsx
+++ client/src/pages/AddBlog.jsx
@@ -1,164 +1,110 @@
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
 import { Formik } from 'formik';
 import * as yup from 'yup'
-import { Container } from 'react-bootstrap';
+import { Container, Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
 
+//using schema for validation from yup. Formik will validate the user input against this using the validationSchema prop. Formik has built in compatibility with yup for this.
 const schema = yup.object().shape({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
+  blogTitle: yup.string().required(),
+  blogText: yup.string().required(),
   username: yup.string().required(),
-  city: yup.string().required(),
-  state: yup.string().required(),
-  zip: yup.string().required(),
   file: yup.mixed().required(),
-  terms: yup.bool().required().oneOf([true], 'terms must be accepted'),
 });
 
 function AddBlog() {
   return (
     <Container>
         <Formik
-      validationSchema={schema}
-      onSubmit={console.log}
-      initialValues={{
-        firstName: 'Mark',
-        lastName: 'Otto',
-        username: '',
-        city: '',
-        state: '',
-        zip: '',
-        file: null,
-        terms: false,
-      }}
-    >
-      {({
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        values,
-        touched,
-        isValid,
-        errors,
-      }) => (
+            validationSchema={schema}
+            onSubmit={console.log}
+            initialValues={{
+                blogTitle: '',
+                blogText: '',
+                username: '',
+                file: null,
+            }}
+        >
+        {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            touched,
+            isValid,
+            errors,
+        }) => (
         <Form noValidate onSubmit={handleSubmit}>
-          <Row className="mb-3">
+            <Row className="mb-3">
             <Form.Group
               as={Col}
               md="4"
               controlId="validationFormik101"
               className="position-relative"
             >
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                type="text"
-                name="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group
-              as={Col}
-              md="4"
-              controlId="validationFormik102"
-              className="position-relative"
-            >
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                type="text"
-                name="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                isValid={touched.lastName && !errors.lastName}
-              />
-
-              <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationFormikUsername2">
-              <Form.Label>Username</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Label>Title</Form.Label>
                 <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  aria-describedby="inputGroupPrepend"
-                  name="username"
-                  value={values.username}
-                  onChange={handleChange}
-                  isInvalid={!!errors.username}
+                    type="text"
+                    name="blogTitle"
+                    placeholder='Enter the title for your post'
+                    value={values.blogTitle}
+                    onChange={handleChange}
+                    isValid={touched.blogTitle && !errors.blogTitle}
+                    isInvalid={!!errors.blogTitle}
                 />
-                <Form.Control.Feedback type="invalid" tooltip>
-                  {errors.username}
+                <Form.Control.Feedback type='valid' tooltip>
+                    Looks good!
                 </Form.Control.Feedback>
-              </InputGroup>
+                <Form.Control.Feedback type='invalid' tooltip>
+                    {errors.blogTitle}
+                </Form.Control.Feedback>
             </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group
-              as={Col}
-              md="6"
-              controlId="validationFormik103"
-              className="position-relative"
+            
+            <Form.Group 
+                className="mt-3 mb-3" 
+                controlId="validationFormik102"
             >
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="City"
-                name="city"
-                value={values.city}
-                onChange={handleChange}
-                isInvalid={!!errors.city}
-              />
-
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.city}
-              </Form.Control.Feedback>
+                <Form.Label>Blog Text</Form.Label>
+                <Form.Control 
+                    as="textarea"
+                    name='blogText'
+                    placeholder='Enter blog text here...' 
+                    rows={10}
+                    value={values.blogText}
+                    onChange={handleChange}
+                    isValid={touched.blogText && !errors.blogText}
+                    isInvalid={!!errors.blogText}
+                />
+                <Form.Control.Feedback type='valid' tooltip>
+                    Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type='invalid' tooltip>
+                    {errors.blogText}
+                </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group
-              as={Col}
-              md="3"
-              controlId="validationFormik104"
-              className="position-relative"
+            
+            <Form.Group 
+                as={Col} 
+                md="4" 
+                controlId="validationFormikUsername2"
             >
-              <Form.Label>State</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="State"
-                name="state"
-                value={values.state}
-                onChange={handleChange}
-                isInvalid={!!errors.state}
-              />
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.state}
-              </Form.Control.Feedback>
+                <Form.Label>Username</Form.Label>
+                <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                    <Form.Control
+                        type="text"
+                        placeholder="Username"
+                        aria-describedby="inputGroupPrepend"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        isInvalid={!!errors.username}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>
+                        {errors.username}
+                    </Form.Control.Feedback>
+                </InputGroup>
             </Form.Group>
-            <Form.Group
-              as={Col}
-              md="3"
-              controlId="validationFormik105"
-              className="position-relative"
-            >
-              <Form.Label>Zip</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Zip"
-                name="zip"
-                value={values.zip}
-                onChange={handleChange}
-                isInvalid={!!errors.zip}
-              />
-
-              <Form.Control.Feedback type="invalid" tooltip>
-                {errors.zip}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
+            </Row>
+          
           <Form.Group className="position-relative mb-3">
             <Form.Label>File</Form.Label>
             <Form.Control
@@ -172,23 +118,11 @@ function AddBlog() {
               {errors.file}
             </Form.Control.Feedback>
           </Form.Group>
-          <Form.Group className="position-relative mb-3">
-            <Form.Check
-              required
-              name="terms"
-              label="Agree to terms and conditions"
-              onChange={handleChange}
-              isInvalid={!!errors.terms}
-              feedback={errors.terms}
-              feedbackType="invalid"
-              id="validationFormik106"
-              feedbackTooltip
-            />
-          </Form.Group>
-          <Button type="submit">Submit form</Button>
+
+          <Button type="submit">Post Blog!</Button>
         </Form>
-      )}
-    </Formik>
+        )}
+        </Formik>
     </Container>
   );
 }
