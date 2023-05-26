086fb42a110167dc35ba5ed7b06d9eaa9e0e9033
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-26 18:17:53 -0400

    setup a simple form to update a users account information using react bootstrap, formik and yup. Working on finishin up this section soon.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,13 @@ import { Navigation } from './components/Navigation'
 import { Homepage } from './pages/Homepage';
 import { PostPage } from './pages/PostPage';
 import { AddBlog } from './pages/AddBlog';
+import { Account } from './pages/Account';
 
 function App() {
   return (
     <>
       <Navigation/>
-      <AddBlog/>
+      <Account/>
     </>
   )
 }
--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,9 +1,105 @@
 import { Container } from "react-bootstrap";
+import { Sidebar } from "../components/Sidebar"
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import * as formik from 'formik';
+import * as yup from 'yup';
 
 function Account(){
+    //using formik to handle submitting and managing forms
+    const { Formik } = formik;
+
+    //schema for validating account info entered by our user
+    const userSchema = yup.object().shape({
+        firstName: yup.string().required(),
+        lastName: yup.string().required(),
+    });
+
     return(
         <Container className="account">
-            <h1>this is your account</h1>
+            <div className="accountContent">
+                <div className="settings">
+                    <span className="editAccount">Edit Your Account Info</span>
+                    <span className="deleteAccount">Delete Your Account</span>
+                </div>
+                <Formik
+                    validationSchema={userSchema}
+                    onSubmit={console.log}
+                    initialValues={{
+                        firstName: '',
+                        lastName: '',
+                        username: ''
+                    }}
+                >
+                {({ handleSubmit, handleChange, values, touched, errors }) => (
+                <Form noValidate onSubmit={handleSubmit}>
+                    <Form.Group as={Col} md="4" controlId="validationFormik01">
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="firstName"
+                            value={values.firstName}
+                            onChange={handleChange}
+                            isValid={touched.firstName && !errors.firstName}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group as={Col} md="4" controlId="validationFormik02">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="lastName"
+                            value={values.lastName}
+                            onChange={handleChange}
+                            isValid={touched.lastName && !errors.lastName}
+                        />
+
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+                        <Form.Label>Username</Form.Label>
+                        <InputGroup hasValidation>
+                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                            <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                aria-describedby="inputGroupPrepend"
+                                name="username"
+                                value={values.username}
+                                onChange={handleChange}
+                                isInvalid={!!errors.username}
+                                />
+                            <Form.Control.Feedback type="invalid">
+                                {errors.username}
+                            </Form.Control.Feedback>
+                        </InputGroup>
+                    </Form.Group>
+                    
+                    <Form.Group className="position-relative mb-3">
+                        <Form.Label>File</Form.Label>
+                        <Form.Control
+                            type="file"
+                            required
+                            name="file"
+                            onChange={handleChange}
+                            isInvalid={!!errors.file}
+                        />
+                        <Form.Control.Feedback type="invalid" tooltip>
+                            {errors.file}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+          
+                    <Button type="submit" className="mt-3">Submit form</Button>
+                </Form>
+                )}
+                </Formik>
+            </div>
+            <Sidebar />
         </Container>
     )
 }
