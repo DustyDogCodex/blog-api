c37d3f0845784ee9c4861f39047a80dccee95017
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-27 15:59:53 -0400

    added browser router for setting up routes. Added a ton of conditional rendering to make sure certain pages are only displayed when a user is logged in. The front-end is finished. Starting work on api stuff next.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,46 @@ import { AddBlog } from './pages/AddBlog';
 import { Account } from './pages/Account';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  //using user variable to test design for logged in users and for having no users
+  //if we have a logged in user, certain pages like register/login will redirect to homepage. This is because if we already have a user logged in, it makes no sense to show them these pages.
+  //similarly, addBlog page will only be showed to users that are logged in to prevent anonymous posts.
+
+  const user = false;
+
   return (
     <>
       <Navigation/>
-      <Register/>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path='/' 
+            element={ <Homepage/> }
+          />
+          <Route 
+            path='register' 
+            element={ user ? <Homepage/> : <Register/> }
+          />
+          <Route 
+            path='login' 
+            element={ user ? <Homepage/> : <Login/> }
+          />
+          <Route 
+            path='post/:id' 
+            element={ <PostPage/> }
+          />
+          <Route 
+            path='account' 
+            element={ user ? <Account/> : <Login/> }
+          />
+          <Route 
+            path='addblog' 
+            element={ user ? <AddBlog/> : <Register/> }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,17 +1,20 @@
-import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap"
+import { Navbar, Container, NavDropdown, Nav, NavLink } from "react-bootstrap"
 
 function Navigation(){
+    //same as in our browser router, certain things in the nav bar will be hidden depending one whether a user is logged in or not
+    const user = false;
+
     return(
         <Navbar bg="light" expand="lg" sticky="top">
             <Container>
-                <Navbar.Brand href="#home">Bloggy</Navbar.Brand>
+                <Navbar.Brand href="/">Bloggy</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto d-flex align-items-center justify-content-center">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="/">About</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
-                        <Nav.Link href="#write">Write</Nav.Link>
+                        <Nav.Link href="/addblog">Write</Nav.Link>
+
                         <NavDropdown title="Socials" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Twitter</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">
@@ -23,12 +26,23 @@ function Navigation(){
                                 Separated link
                             </NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="#account">
-                            <img 
-                                className="navbarImage" 
-                                src="https://e1.pxfuel.com/desktop-wallpaper/467/133/desktop-wallpaper-pin-on-anime-y-mas-anime-avatar-girl.jpg" 
-                                alt="avatar" 
-                            />
+
+                        <Nav.Link href="/account">
+                            { 
+                            user 
+                            ?   <div style={{display:'flex', alignItems:'center'}}>
+                                    <img 
+                                        className="navbarImage" 
+                                        src="https://e1.pxfuel.com/desktop-wallpaper/467/133/desktop-wallpaper-pin-on-anime-y-mas-anime-avatar-girl.jpg" 
+                                        alt="user avatar" 
+                                    />
+                                    <Nav.Link href="#logout">Logout</Nav.Link>
+                                </div>
+                            :   <div style={{display:'flex'}}>
+                                    <Nav.Link href="/login">Login</Nav.Link> 
+                                    <Nav.Link href="/register">Register</Nav.Link>
+                                </div>
+                            }
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
