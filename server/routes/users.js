036693a5987c75b21fb03f3dcc637ff28de52f8e
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-29 13:38:11 -0400

    added a route for deleting user account and all associated posts with it. Tested it postman and its working well.

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,11 +4,12 @@ const bcrypt = require('bcrypt')
 const Router = express.Router()
 
 const User = require('../models/Users')
+const Post = require('../models/Posts')
 
 //Route for updating user information
 Router.put(
     '/:id', 
-    asyncHandler( async(req,res,next) => {
+    asyncHandler(async(req,res,next) => {
         if(req.body.userId === req.params.id){
             //checking for an update password. If user is sending a new password, this block will hash it before storing
             if(req.body.password){
@@ -28,10 +29,30 @@ Router.put(
     })
 )
 
-
-
 //Route for deleting user account
+Router.delete(
+    '/:id', 
+    asyncHandler(async(req,res,next) => {
+        if(req.body.userId === req.params.id){
+            //find selected user account in our database
+            const user = await User.findById(req.params.id)
+            if(user){
+                //if user account is found
+                //first we delete all associated posts from that account
+                await Post.deleteMany({ username: user.username })
 
-
+                //then we delete selected user account by deleting its document
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json('User account has been deleted!')
+            } else {
+                res.status(404).json('User account was not found!')
+            }
+            
+        } else {
+            //if userId is different from params, this is not the correct users account!
+            res.status(401).json("You are not authorised to delete this account!")
+        }
+    })
+)
 
 module.exports = Router
