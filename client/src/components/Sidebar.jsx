fd0e3d711646829859048642c53006e0ce7edbc4
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-30 15:57:38 -0400

    category elements are not being rendered atm. WIll come back to this to fix this later.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 function Sidebar(){
+
+    //using state to update list of available catgories in the sidebar
+    const [categories, setCategories] = useState([])
+
+    //fetching available categories
+    useEffect(() => {
+        const getCategories = async() => {
+            const res = await axios.get('http://localhost:5000/category')
+            setCategories(res.data)
+            console.log('these are our categories',categories)
+        }
+        getCategories()
+    },[])
+
+    //mapping categories into an array for display
+    let categoriesElements = categories.map((cat,index) => {
+                        <li key={index} className="sidebarListItem">{cat.name}</li>
+                    })
+
     return(
         <div className="sidebar">
             <div className="sidebarItem">
@@ -8,17 +30,14 @@ function Sidebar(){
                     src="https://e1.pxfuel.com/desktop-wallpaper/467/133/desktop-wallpaper-pin-on-anime-y-mas-anime-avatar-girl.jpg" 
                     alt="avatar" 
                 />
-                <p style={{margin:'15px'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, necessitatibus quo hic ratione earum dolore sunt consectetur, beatae reiciendis non, nihil magni maiores tenetur quaerat possimus amet debitis! Ex, accusantium?</p>
+                <p style={{margin:'15px'}}>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, necessitatibus quo hic ratione earum dolore sunt consectetur, beatae reiciendis non, nihil magni maiores tenetur quaerat possimus amet debitis! Ex, accusantium?
+                </p>
             </div>
             <div className="sidebarItem">
                 <span className="sidebarTitle">Categories</span>
                 <ul className="sidebarList">
-                    <li className="sidebarListItem">Life</li>
-                    <li className="sidebarListItem">Music</li>
-                    <li className="sidebarListItem">Sports</li>
-                    <li className="sidebarListItem">Nature</li>
-                    <li className="sidebarListItem">Tech</li>
-                    <li className="sidebarListItem">Movies</li>
+                    { categoriesElements }
                 </ul>
             </div>
         </div>
