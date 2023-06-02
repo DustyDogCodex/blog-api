2a8b78dd9e58cc8907c76308b9c8d4b5a306c86a
Varun Malaviya <varunmalaviya7@gmail.com>
2023-06-02 17:43:41 -0400

    removed formik from login and register pages. set up usestate to control form control elements and handle changes. Setup a handlesubmit fucntion that will toggle a dismissible bootstrap alert if the fields are left empty. Setting up post requests to submit info to our backend next.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,26 +10,50 @@ function Login(){
     const [usernameAlert, setUsernameAlert] = useState(false)
     const [passwordAlert, setPasswordAlert] = useState(false)
 
-    //handling submit
-    function handleSubmit(){
+    //onChange handling functions
+    function changeUsername(e){
+        setUsername(e.target.value)
+    }
+    function changePassword(e){
+        setPassword(e.target.value)
+    }
+
+    //handling submit. this will also toggle bootstrap alerts is any field is left empty
+    function handleSubmit(e){
+        e.preventDefault()
+        if(!username || !password){
+            username.length === 0 ? setUsernameAlert(true) : ''
+            password.length === 0 ? setPasswordAlert(true) : '' 
+        }
         console.log(username,password)
     }
 
     return(
         <div className="login">
-                <h1 style={{fontFamily:'Permanent Marker, cursive', fontSize:'60px', marginBottom:'30px'}}>Login</h1>
-                <Form className="d-flex flex-column">
+                <h1 style={{
+                    fontFamily:'Permanent Marker, cursive', 
+                    fontSize:'60px', 
+                    marginBottom:'30px'}}>
+                    Login
+                </h1>
+                <Form className="d-flex flex-column" onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Username</Form.Label>
                         <Form.Control 
                             type="text"
                             name="username" 
                             value={username}
+                            onChange={changeUsername}
                             placeholder="Enter your username" 
                         />
-                        <Alert variant="danger" show={usernameAlert} onClose={() => setUsernameAlert(false)} dismissible>
-                           
-                        </Alert>  
+                        <Alert 
+                            variant="danger" 
+                            show={usernameAlert} 
+                            className="mt-3"
+                            onClose={() => setUsernameAlert(false)} 
+                            dismissible >
+                            Username is required
+                        </Alert>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -38,12 +62,17 @@ function Login(){
                             type="password"
                             name="password" 
                             value={password}
+                            onChange={changePassword}
                             placeholder="Enter your password" 
                         />
-                        <Alert variant="danger" show={passwordAlert} onClose={() => setPasswordAlert(false)} dismissible>
-                            
+                        <Alert 
+                            variant="danger" 
+                            show={passwordAlert} 
+                            className="mt-3"
+                            onClose={() => setPasswordAlert(false)} 
+                            dismissible >
+                            Password is required
                         </Alert>
-
                     </Form.Group>
 
                     <Button variant="primary" type="submit">
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,19 +1,78 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap"
+import { useState } from "react"
 
 function Register(){
+    //using state variables to keep track of user input.
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    //using state to toggle dismissible bootstrap alerts.
+    const [usernameAlert, setUsernameAlert] = useState(false)
+    const [passwordAlert, setPasswordAlert] = useState(false)
+
+    //onChange handling functions
+    function changeUsername(e){
+        setUsername(e.target.value)
+    }
+    function changePassword(e){
+        setPassword(e.target.value)
+    }
+
+    //handling submit. this will also toggle bootstrap alerts is any field is left empty
+    function handleSubmit(e){
+        e.preventDefault()
+        if(!username || !password){
+            username.length === 0 ? setUsernameAlert(true) : ''
+            password.length === 0 ? setPasswordAlert(true) : '' 
+        }
+        console.log(username,password)
+    }
 
     return(
         <div className="login">
-            <h1 style={{fontFamily:'Permanent Marker, cursive', fontSize:'60px', marginBottom:'30px'}}>Create a New Account</h1>
-            <Form className="d-flex flex-column">
+            <h1 style={{ 
+                fontFamily:'Permanent Marker, cursive', 
+                fontSize:'60px', 
+                marginBottom:'30px'}}>
+                Create a New Account
+            </h1>
+            <Form className="d-flex flex-column" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your username" />
+                    <Form.Control 
+                        type="text" 
+                        name="username"
+                        value={username}
+                        onChange={changeUsername}
+                        placeholder="Enter your username" 
+                    />
+                    <Alert 
+                        variant="danger" 
+                        show={usernameAlert} 
+                        className="mt-3"
+                        onClose={() => setUsernameAlert(false)} 
+                        dismissible >
+                        Username is a required field!
+                    </Alert> 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter your password" />
+                    <Form.Control 
+                        type="password" 
+                        name="password"
+                        value={password}
+                        onChange={changePassword}
+                        placeholder="Enter your password" 
+                    />
+                    <Alert 
+                        variant="danger" 
+                        show={passwordAlert}
+                        className="mt-3" 
+                        onClose={() => setPasswordAlert(false)} 
+                        dismissible >
+                        Password is a required field!
+                    </Alert>
                 </Form.Group>
     
                 <Button variant="primary" type="submit">
