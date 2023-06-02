a00e0f968980a9b5f169c6cfac74a9b2fc626d9f
Varun Malaviya <varunmalaviya7@gmail.com>
2023-06-02 17:22:36 -0400

    removed formik as it was toggling error alerts even when there were no errors. I think it has something to do with formik intefering with react bootstrap form controls or the other way round. I can't figure out a way to see which of them is the problem and this is a very small form(only 2 inputs) so its actually easier and quicker to use useState to tarck input and then right a small fucntion to validate it client side.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,18 +1,14 @@
 import { Form, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
-import * as formik from 'formik';
-import * as yup from 'yup';
 
 function Login(){
-    const { Formik } = formik
+    //using state variables to keep track of user input.
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    //using state to toggle dismissible bootstrap alerts.
     const [usernameAlert, setUsernameAlert] = useState(false)
     const [passwordAlert, setPasswordAlert] = useState(false)
-
-    //using schema for validation from yup. Formik will validate the user input against this using the validationSchema prop. Formik has built in compatibility with yup for this.
-    const loginSchema = yup.object().shape({
-        username: yup.string().required('Required'),
-        password: yup.string().required('Required')
-    });
 
     //handling submit
     function handleSubmit(){
@@ -20,34 +16,7 @@ function Login(){
     }
 
     return(
-        <div className="login" style={{position:'relative'}}>
-            <Formik
-                validationSchema={loginSchema} 
-                initialValues={{ username: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.username) {
-                        errors.username = 'Username is required';
-                        setUsernameAlert(true)
-                    } 
-                    if (!values.password) {
-                        errors.password = 'Password is required';
-                        setPasswordAlert(true)
-                    } 
-                    return errors;
-                }}
-                onSubmit={handleSubmit}
-            >
-            {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-            }) => (
-                <>
+        <div className="login">
                 <h1 style={{fontFamily:'Permanent Marker, cursive', fontSize:'60px', marginBottom:'30px'}}>Login</h1>
                 <Form className="d-flex flex-column">
                     <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -55,18 +24,11 @@ function Login(){
                         <Form.Control 
                             type="text"
                             name="username" 
-                            value={values.username}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
+                            value={username}
                             placeholder="Enter your username" 
-                            isValid={touched.username && !errors.username}
-                            isInvalid={!!errors.username}
                         />
-                        <Form.Control.Feedback type="invalid" tooltip>
-                            {errors.username}
-                        </Form.Control.Feedback>
                         <Alert variant="danger" show={usernameAlert} onClose={() => setUsernameAlert(false)} dismissible>
-                            {errors.username && touched.username && errors.username}
+                           
                         </Alert>  
                     </Form.Group>
 
@@ -75,18 +37,11 @@ function Login(){
                         <Form.Control 
                             type="password"
                             name="password" 
-                            value={values.password}
-                            onChange={handleChange} 
-                            onBlur={handleBlur}
+                            value={password}
                             placeholder="Enter your password" 
-                            isValid={touched.password && !errors.password}
-                            isInvalid={!!errors.password}
                         />
-                        <Form.Control.Feedback type="invalid" tooltip>
-                            {errors.password}
-                        </Form.Control.Feedback>
                         <Alert variant="danger" show={passwordAlert} onClose={() => setPasswordAlert(false)} dismissible>
-                            {errors.password && touched.password && errors.password}
+                            
                         </Alert>
 
                     </Form.Group>
@@ -99,9 +54,6 @@ function Login(){
                 <Button variant="success" type="submit">
                     Register
                 </Button>
-                </>
-            )}
-            </Formik>
         </div>
     )
 }
