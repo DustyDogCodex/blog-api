c1fdc9a0e44537eb13d78e2df880c8d90c89bda8
vmalaviya <varunmalaviya7@gmail.com>
2023-08-19 14:03:52 -0400

    fixed buggy navbar. Now navbar expands smoothly instead of being stuck in a square on the corner

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -51,66 +51,57 @@ function Navigation(){
                 <Navbar.Brand href="/">Bloggy</Navbar.Brand>
 
                 {/* collapsible nav menu */}
-                <div>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav
-                            className="me-auto d-flex align-items-center justify-content-center"
-                        >
-                            <Nav.Link href="/about">Who We Are</Nav.Link>
-                            <Nav.Link href="/addblog">Write</Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav
+                        className="me-auto d-flex align-items-center justify-content-center"
+                    >
+                        <Nav.Link href="/about">Who We Are</Nav.Link>
+                        <Nav.Link href="/addblog">Write</Nav.Link>
+                        {/* conditional rendering of login/account button */}
+                        {userInfo 
+                            ?   
+                            /* if user is logged in, display avatar and link to account */
+                            <>
+                                {/* link to user dashboard */}
+                                <Nav.Link href="/dashboard">
+                                    {/* conditionally rendering avatar or icon */}
+                                    {userInfo.avatar
+                                        ?
+                                        <img 
+                                            style={{ width:'2rem', height:'2rem', borderRadius:'100%' }}
+                                            src={`http://localhost:5000/uploads/${userInfo.avatar}`}
+                                            alt="user avatar" 
+                                        />
+                                        :
+                                        <FontAwesomeIcon 
+                                            icon={faUser} 
+                                            style={{ color:'white', height:'1.5rem', width:'1.5rem' }} 
+                                        />
+                                    }                                            
+                                </Nav.Link>
 
-                            <Nav.Item>
-                                {/* conditional rendering of login/account button */}
-                                { 
-                                userInfo 
-                                    ?   
-                                    /* if user is logged in, display avatar and link to account */
-                                    <div 
-                                        style={{display:'flex', alignItems:'center'}}
-                                    >
-                                        <Nav.Link href="/dashboard">
-                                            {/* conditionally rendering avatar or icon */}
-                                            {userInfo.avatar
-                                                ?
-                                                (
-                                                    <img 
-                                                        style={{ width:'2rem', height:'2rem', borderRadius:'100%' }}
-                                                        src={`http://localhost:5000/uploads/${userInfo.avatar}`}
-                                                        alt="user avatar" 
-                                                    />
-                                                )
-                                                :
-                                                <FontAwesomeIcon 
-                                                    icon={faUser} 
-                                                    style={{ color:'white', height:'1.5rem', width:'1.5rem' }} 
-                                                />
-                                            }                                            
-                                        </Nav.Link>
-
-                                        {/* Logout button */}
-                                        <Button 
-                                            variant="danger" 
-                                            type='button'
-                                            onClick={logout}
-                                        >
-                                            Logout
-                                        </Button>
-                                    </div>
-                                    :   
-                                    /* if no logged in user, links to login and register */
-                                    <div style={{ display:'flex' }}>
-                                        <Nav.Link href="/login">Login</Nav.Link> 
-                                        <Nav.Link href="/register">Register</Nav.Link>
-                                    </div>
-                                }
-                            </Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </div>
+                                {/* Logout button */}
+                                <Button 
+                                    variant="danger" 
+                                    type='button'
+                                    onClick={logout}
+                                >
+                                    Logout
+                                </Button>
+                            </>
+                            :   
+                            /* if no logged in user, links to login and register */
+                            <>
+                                <Nav.Link href="/login">Login</Nav.Link> 
+                                <Nav.Link href="/register">Register</Nav.Link>
+                            </>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
-    </>
+        </>
     )
 }
 
